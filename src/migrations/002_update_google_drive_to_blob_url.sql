20ceb37a801b2b2d8f5b8d023960d2e57ed2d734
Aaron Peazzoni <peazzoni@yahoo.com>
2025-09-30 20:34:41 -0400

    Fix DB migrations

--- src/migrations/002_update_google_drive_to_blob_url.sql
+++ src/migrations/002_update_google_drive_to_blob_url.sql
@@ -5,10 +5,16 @@
 ALTER TABLE audit_requests 
 ADD COLUMN IF NOT EXISTS blob_url VARCHAR(2048);
 
--- Copy existing data from google_drive_url to blob_url
-UPDATE audit_requests 
-SET blob_url = google_drive_url 
-WHERE google_drive_url IS NOT NULL;
+-- Copy existing data from google_drive_url to blob_url (if column exists)
+DO $$
+BEGIN
+  IF EXISTS (SELECT 1 FROM information_schema.columns 
+             WHERE table_name = 'audit_requests' AND column_name = 'google_drive_url') THEN
+    UPDATE audit_requests 
+    SET blob_url = google_drive_url 
+    WHERE google_drive_url IS NOT NULL;
+  END IF;
+END $$;
 
 -- Drop old google_drive_url column
 ALTER TABLE audit_requests 
